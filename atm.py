0b346da20d0df2fb3588d25eac23ef1cea93fc3f
YSH1214 <hktysh@gmail.com>
2025-07-18 13:25:04 +0900

    ✨ 영수증 보기 기능 추가
    사용자가 영수증 내역을 확인할 수 있는 기능 추가 및 영수증 내역 없을 시 안내 메시지 작성

--- atm.py
+++ atm.py
@@ -5,8 +5,9 @@
 # 기능 입력을 받는 기능을 구현해주세요
 
 balance = 10000
+# 사용하기 위해 미리 만드는것, 초기화 특정값을 넣어서 만들거나, 빈 값을 넣어서 만듭니다.
+receipts = []
 
-#무한루프? while  vs for
 while True:
     num = input("사용하실 기능의 번호를 선택해주세요 (1.입금, 2.출금, 3.영수증 보기, 4.종료) : ")
 
@@ -17,14 +18,23 @@ while True:
        deposit_amount = int(input('입금할 금액을 입력해주세요: ')) # str:5000 -> int() -> int:5000
        balance += deposit_amount # balance = 10000 + deposit_amount
        print(f'입금하신 금액{deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+       receipts.append(('입금', deposit_amount, balance))
 
     if num == '2':
         withdraw_amount = int(input("출금할 금액을 입력해주세요: "))
         withdraw_amount = min(balance,withdraw_amount)
         balance -= withdraw_amount # balance = 10000 - withdraw_amount(5000)
         print(f"출금하신 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+        receipts.append(('출금', withdraw_amount, balance))
 
     if num == '3':
-        pass
+        if receipts: #('출금', 5000, 8000)
+            # [('입금', 3000, 13000), ('출금', 5000, 8000), ('입금', 100000, 108000), ('입금', 1000000, 1108000)]
+            for i in receipts:
+                # i = ('출금', 5000, 8000)
+                # i[0] => '출금'
+                print(f"{i[0]}: {i[1]}원 | 잔액: {i[2]}")
+        else:
+            print("영수증 내역이 없습니다.")
 
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}원 입니다.')
